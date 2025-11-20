4aef417926317d5f5b57ed293ce009bcad13c612
Luis Guzman <lumigu.dev@gmail.com>
2025-11-19 20:59:19 -0500

    ♻️ Refactor: Replace async database engine with synchronous implementation in db.py

--- Backend/app/core/db.py
+++ Backend/app/core/db.py
@@ -1,14 +1,11 @@
 from typing import Annotated
 
 from fastapi import Depends
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.orm import sessionmaker
-from sqlmodel import SQLModel
-from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import Session, create_engine
 
 from app.core.config import settings
 
-engine = create_async_engine(
+engine = create_engine(
     str(settings.DATABASE_URI),
     # !imprime logs de querys
     echo=settings.ENVIRONMENT == "development",
@@ -16,21 +13,9 @@ engine = create_async_engine(
 )
 
 
-async def init_db() -> None:
-    async with engine.begin() as conn:
-        # run_sync permite ejecutar operaciones sincrónicas como create_all dentro de una sesión asíncrona
-        await conn.run_sync(SQLModel.metadata.create_all)
-
-
-async def get_session():
-    async_session = sessionmaker(
-        bind=engine,
-        class_=AsyncSession,
-        expire_on_commit=False,
-    )
-
-    async with async_session() as session:
+def get_session():
+    with Session(engine) as session:
         yield session
 
 
-SessionDep = Annotated[AsyncSession, Depends(get_session)]
+SessionDep = Annotated[Session, Depends(get_session)]
