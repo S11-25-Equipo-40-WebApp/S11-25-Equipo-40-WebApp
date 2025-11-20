611ea57e452c1aca6dbf6dcb1ca2441a6f899812
Luis Guzman <lumigu.dev@gmail.com>
2025-11-19 20:55:48 -0500

    🔥 Refactor: Remove lifespan context manager and database initialization from FastAPI app

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,21 +1,10 @@
 from fastapi import FastAPI
-from fastapi.concurrency import asynccontextmanager
 
 from app.core.config import settings
-from app.core.db import init_db
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Startup event
-    await init_db()
-    yield
-
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version="0.0.1",
-    lifespan=lifespan,
 )
 
 
