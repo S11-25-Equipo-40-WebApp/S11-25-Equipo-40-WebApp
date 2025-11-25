25d58761c45b6e93664bba05b54b074eebbdba03
Luis Guzman <lumigu.dev@gmail.com>
2025-11-25 18:51:54 -0500

    ✨ feat: Initialize schema imports in __init__.py

---
+++ Backend/app/schemas/__init__.py
@@ -0,0 +1,37 @@
+from .category import (
+    CategoryCreate,
+    CategoryResponse,
+    CategoryUpdate,
+)
+from .tag import (
+    TagCreate,
+    TagResponse,
+    TagUpdate,
+)
+from .testimonial import (
+    TestimonialCreate,
+    TestimonialResponse,
+    TestimonialUpdate,
+)
+from .user import (
+    UserCreate,
+    UserLogin,
+    UserResponse,
+    UserUpdate,
+)
+
+__all__ = [
+    "CategoryCreate",
+    "CategoryResponse",
+    "CategoryUpdate",
+    "TagCreate",
+    "TagResponse",
+    "TagUpdate",
+    "TestimonialCreate",
+    "TestimonialResponse",
+    "TestimonialUpdate",
+    "UserCreate",
+    "UserResponse",
+    "UserUpdate",
+    "UserLogin",
+]
