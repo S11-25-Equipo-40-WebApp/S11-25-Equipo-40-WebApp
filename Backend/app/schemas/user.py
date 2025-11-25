e2e05d3349c83bfbb8c7476b9ba08131c5785ed5
Luis Guzman <lumigu.dev@gmail.com>
2025-11-25 18:51:14 -0500

    ✨ feat: Enhance password validation in UserCreate schema

--- Backend/app/schemas/user.py
+++ Backend/app/schemas/user.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import EmailStr
+from pydantic import EmailStr, field_validator
 from sqlmodel import Field, SQLModel
 
 from app.models.user import Roles
@@ -11,8 +11,24 @@ class UserCreate(SQLModel):
     password: str = Field(
         min_length=8,
         max_length=128,
-        regex=r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?])[A-Za-z\d!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?]{8,}$",
     )
+
+    @field_validator("password")
+    def validate_password(cls, v: str):
+        if not any(c.islower() for c in v):
+            raise ValueError("La contraseña debe incluir al menos una letra minúscula.")
+
+        if not any(c.isupper() for c in v):
+            raise ValueError("La contraseña debe incluir al menos una letra mayúscula.")
+
+        if not any(c.isdigit() for c in v):
+            raise ValueError("La contraseña debe incluir al menos un número.")
+
+        special = "!@#$%^&*()_+-=[]{};':\"\\|,.<>/?"
+        if not any(c in special for c in v):
+            raise ValueError(f"La contraseña debe incluir al menos un carácter especial: {special}")
+
+        return v
 
 
 class UserLogin(UserCreate):
