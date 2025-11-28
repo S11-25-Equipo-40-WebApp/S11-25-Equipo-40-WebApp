b2a3e0470aafc0a57bd8b68df0078061e7f6e43b
Cesar <117487764+sibas1@users.noreply.github.com>
2025-11-28 11:24:21 -0500

    ✨ feat: add JWT access token generator with expiration

---
+++ Backend/app/core/jwt.py
@@ -0,0 +1,14 @@
+from datetime import UTC, datetime, timedelta
+
+from jose import jwt
+
+from app.core.config import settings
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+
+    expire = datetime.now(UTC) + (expires_delta or timedelta(hours=6))
+    to_encode["exp"] = expire
+
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
