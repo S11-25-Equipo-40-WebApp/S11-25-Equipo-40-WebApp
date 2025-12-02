0a568e24ce14b7737f91e971fc81f3cdad8adf26
Cesar <117487764+sibas1@users.noreply.github.com>
2025-12-02 00:18:27 -0500

    ✨  Feat: ladd create_refresh_token

--- Backend/app/core/jwt.py
+++ Backend/app/core/jwt.py
@@ -7,8 +7,13 @@ from app.core.config import settings
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
-
-    expire = datetime.now(UTC) + (expires_delta or timedelta(hours=6))
+    expire = datetime.now(UTC) + (expires_delta or timedelta(minutes=30))
     to_encode["exp"] = expire
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
+
+def create_refresh_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + (expires_delta or timedelta(days=30))
+    to_encode["exp"] = expire
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
