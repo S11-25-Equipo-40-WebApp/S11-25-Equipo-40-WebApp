f19c12d2d0d8dace4843889d690ff36dfb735fbb
Luis Guzman <lumigu.dev@gmail.com>
2025-12-03 13:19:26 -0500

    ✨ feat: include API key router in main API router

--- Backend/app/api/index.py
+++ Backend/app/api/index.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
+from app.api.router.api_key import router as api_key_router
 from app.api.router.auth import router as auth_router
 from app.api.router.user import router as user_router
 
 router = APIRouter()
 router.include_router(auth_router)
 router.include_router(user_router)
+router.include_router(api_key_router)
