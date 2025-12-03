852d4d3e34be96993693bbb056de15ea46fb945e
Cesar <117487764+sibas1@users.noreply.github.com>
2025-12-03 13:14:11 -0500

    ✨ Fix: Chage REFRESH_SECRET_KEY token refrsh

--- Backend/app/core/jwt.py
+++ Backend/app/core/jwt.py
@@ -16,4 +16,4 @@ def create_refresh_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     expire = datetime.now(UTC) + (expires_delta or timedelta(days=30))
     to_encode["exp"] = expire
-    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return jwt.encode(to_encode, settings.REFRESH_SECRET_KEY, algorithm=settings.ALGORITHM)
