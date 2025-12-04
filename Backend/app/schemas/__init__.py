5f8fbb221555e5385678b14672e5a2174243e55a
Luis Guzman <lumigu.dev@gmail.com>
2025-12-04 13:38:07 -0500

    ✨ new: streamline import statements in schema module

--- Backend/app/schemas/__init__.py
+++ Backend/app/schemas/__init__.py
@@ -1,33 +1,10 @@
-from .api_key import (
-    APIKeyCreate,
-    APIKeyListResponse,
-    APIKeyResponse,
-    APIKeyUpdate,
-)
-from .category import (
-    CategoryCreate,
-    CategoryResponse,
-    CategoryUpdate,
-)
-from .tag import (
-    TagCreate,
-    TagResponse,
-    TagUpdate,
-)
-from .testimonial import (
-    TestimonialCreate,
-    TestimonialResponse,
-    TestimonialUpdate,
-)
-from .token import (
-    TokenResponse,
-)
-from .user import (
-    UserCreate,
-    UserLogin,
-    UserResponse,
-    UserUpdate,
-)
+from .api_key import APIKeyCreate, APIKeyListResponse, APIKeyResponse, APIKeyUpdate
+from .category import CategoryCreate, CategoryResponse, CategoryUpdate
+from .pagination import PaginationResponse
+from .tag import TagCreate, TagResponse, TagUpdate
+from .testimonial import TestimonialCreate, TestimonialResponse, TestimonialUpdate
+from .token import TokenResponse
+from .user import AdminUserUpdate, UserCreate, UserLogin, UserResponse, UserUpdate
 
 __all__ = [
     "TokenResponse",
@@ -44,8 +21,10 @@ __all__ = [
     "TestimonialCreate",
     "TestimonialResponse",
     "TestimonialUpdate",
+    "AdminUserUpdate",
     "UserCreate",
+    "UserLogin",
     "UserResponse",
     "UserUpdate",
-    "UserLogin",
+    "PaginationResponse",
 ]
