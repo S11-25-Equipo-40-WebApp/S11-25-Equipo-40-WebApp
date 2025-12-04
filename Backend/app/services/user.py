1ad25a3df0db5a5580c094f779b4779108352805
Luis Guzman <lumigu.dev@gmail.com>
2025-12-04 13:47:57 -0500

    ♻️ refactor: refactor user service methods for improved clarity and functionality

--- Backend/app/services/user.py
+++ Backend/app/services/user.py
@@ -1,59 +1,93 @@
-from uuid import uuid4 as UUID
+from uuid import UUID
 
 from fastapi import HTTPException, status
-from sqlmodel import Session, select
+from pydantic import EmailStr
+from sqlmodel import select
 
+from app.core.db import SessionDep
 from app.models.user import User
-from app.schemas.user import UserUpdate
+from app.schemas.pagination import PaginationResponse
+from app.schemas.user import UserResponse
 
 
 class UserService:
     @staticmethod
-    def get_user_list(db=Session):
-        stmt = select(User)
+    def get_users(db: SessionDep, skip: int, limit: int) -> PaginationResponse[UserResponse]:
+        stmt = select(User).offset(skip).limit(limit)
         result = db.exec(stmt)
         user = result.all()
-        return user
+
+        user_responses = [UserResponse.model_validate(u) for u in user]
+
+        return PaginationResponse(
+            total_items=len(user_responses),
+            results=user_responses,
+            page=skip // limit + 1,
+            size=limit,
+            total_pages=(len(user_responses) + limit - 1) // limit,
+            has_next=(skip + limit) < len(user_responses),
+            has_prev=skip > 0,
+        )
 
     @staticmethod
-    def get_user_by_id(db=Session, id=UUID):
-        stmt = select(User).where(User.id == id)
-        result = db.exec(stmt)
-        user = result.one_or_none()
+    def get_user_by_id(db: SessionDep, id: UUID) -> User:
+        user = db.get(User, id)
         if not user:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
         return user
 
     @staticmethod
-    def delete_user(db=Session, id=UUID):
-        stmt = select(User).where(User.id == id)
-        result = db.exec(stmt)
-        user = result.one_or_none()
+    def soft_delete_user(db: SessionDep, id: UUID):
+        user = db.get(User, id)
+
         if not user:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-        db.delete(user)
+
+        user.is_active = False
+        db.add(user)
         db.commit()
-        return user
+
+        return HTTPException(
+            status_code=status.HTTP_204_NO_CONTENT, detail="User soft deleted successfully"
+        )
 
     @staticmethod
-    def update_user(db=Session, data=UserUpdate, id=UUID):
-        stmt = select(User).where(User.id == id)
-        result = db.exec(stmt)
-        user = result.one_or_none()
+    def update_user(db: SessionDep, user_id: UUID, data):
+        user = db.get(User, user_id)
         if not user:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-        for key, value in data.model_dump(exclude_unset=True).items():
+            raise HTTPException(status_code=404, detail="User not found")
+
+        update_data = data.dict(exclude_unset=True)
+
+        for key, value in update_data.items():
             setattr(user, key, value)
+
         db.add(user)
         db.commit()
         db.refresh(user)
         return user
 
     @staticmethod
-    def get_user_by_email(db=Session, email=str):
+    def get_user_by_email(db: SessionDep, email: EmailStr):
         stmt = select(User).where(User.email == email)
         result = db.exec(stmt)
-        user = result.one_or_none()
+        user = result.first()
         if not user:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
         return user
+
+    # @staticmethod
+    # def update_user_role(
+    #     user_id: UUID,
+    #     new_role: AdminUserUpdate,
+    #     db: SessionDep,
+    # ) -> User:
+    #     user = db.get(User, user_id)
+
+    #     if not user:
+    #         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    #     user.role = new_role.new_role
+    #     db.add(user)
+    #     db.commit()
+    #     db.refresh(user)
+    #     return user
