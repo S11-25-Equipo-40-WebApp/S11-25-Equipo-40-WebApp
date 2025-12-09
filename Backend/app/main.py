49aa77d2216280b83d5e9ca5cfd878efaee4499a
Luis Guzman <lumigu.dev@gmail.com>
2025-12-09 09:55:27 -0500

    ✨ feat: add CORS middleware configuration for development and production environments

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.index import router as api_router
 from app.core.config import settings
@@ -9,6 +10,23 @@ app = FastAPI(
 )
 
 app.include_router(api_router, prefix=settings.API)
+
+if settings.ENVIRONMENT == "development":
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+else:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 @app.get("/")
